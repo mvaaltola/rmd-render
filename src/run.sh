46aa00c738d0d3a7b1ae336857e986174e03d509
Mikael Vaaltola <mvaaltola@gmail.com>
2021-10-20 15:33:35 +0300

    Start working on custom template

--- src/run.sh
+++ src/run.sh
@@ -4,7 +4,7 @@ set -e
 
 rm -rf /out/*
 
-Rscript -e "bookdown::render_book('/work', 'bookdown::html_chapters')"
+Rscript -e "bookdown::render_book('/work', 'bookdown::html_book')"
 
 mv _book/* /out/
 rm -r _book
